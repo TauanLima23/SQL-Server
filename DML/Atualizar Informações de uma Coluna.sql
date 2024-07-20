847176265d1159db0cb7e8ea51d8b55a4aa110de
Tauan Lima <contato@tauanlima.com.br>
2024-07-20 15:45:06 -0300

    Create Update column

---
+++ DML/Atualizar Informações de uma Coluna.sql
@@ -0,0 +1,12 @@
+/*
+Para atualizar informações em uma coluna específica de uma tabela no SQL Server, você usa a instrução UPDATE. Esta instrução permite modificar os dados em uma ou mais colunas de uma ou mais linhas em uma tabela. Aqui está a estrutura básica da instrução UPDATE
+*/
+
+UPDATE nome_da_tabela
+SET nome_da_coluna = novo_valor
+WHERE condicao;
+
+
+/*
+Também é possível utilizar o FROM antes do WHERE  para realizar JOIN entre tabelas 
+*/
